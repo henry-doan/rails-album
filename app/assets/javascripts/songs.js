5b6b5b006541bb2ec30d3c1d176593f5ccb437fa
Henry Doan <henrydoan96@gmail.com>
2018-03-05 14:53:42 -0700

    delete working

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
   $('.collection').sortable();
   $('.modal').modal();
+
+  // TODO delete to work on without refresh
+  $(document).on('click', '#delete-song', function () {
+    var id = $(this).parent('.song').data().id
+    $.ajax({
+      url: '/songs/' + id,
+      method: 'DELETE'
+    }).done(function () {
+      var row = $("[data-id='" + id + "'");
+      row.parent().remove('.song');
+    }).fail(function (err) {
+      console.log(err)
+    })
+  })
 });
