53756bcf9de4bd8ec5da4214bbf24bc544c46137
Henry Doan <henrydoan96@gmail.com>
2018-03-06 20:19:52 -0700

    update and delete works

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -1,18 +1,4 @@
 $(document).ready(function () {
   $('.collection').sortable();
   $('.modal').modal();
-
-  // TODO delete to work on without refresh
-  $(document).on('click', '#delete-song', function () {
-    var id = $(this).parent('.song').data().id
-    $.ajax({
-      url: '/songs/' + id,
-      method: 'DELETE'
-    }).done(function () {
-      var row = $("[data-id='" + id + "'");
-      row.parent().remove('.song');
-    }).fail(function (err) {
-      console.log(err)
-    })
-  })
 });
